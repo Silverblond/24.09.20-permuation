c110d5f0e7f8568a43b4ea186f1b291944cb2263
DongGyun Kim <kdk23538@gmail.com>
2024-09-23 23:20:05 +0900

    First commit 1st completion

---
+++ cmake-build-debug/CMakeFiles/24_09_20_permutaion.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "24_09_20_permutaion.exe"
+  "24_09_20_permutaion.exe.manifest"
+  "24_09_20_permutaion.pdb"
+  "CMakeFiles/24_09_20_permutaion.dir/main.c.obj"
+  "CMakeFiles/24_09_20_permutaion.dir/main.c.obj.d"
+  "lib24_09_20_permutaion.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/24_09_20_permutaion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,114 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#define SWAP(x,y,t) ((t)=(x), (x)=(y), (y)=(t))
+
+//순열을 생성하는 함수
+void perm(char *list, int i, int n) {
+    int j, temp;
+    if (i == n) {
+        for (j = 0; j <= n; j++) {
+            printf("%c", list[j]);
+        }
+        printf("   ");
+    }
+    else {
+        for(j=i; j<=n; j++) {
+            //j와 n의 값을 SWAP, 그리고 다시 SWAP으로 원상복구
+            SWAP(list[i], list[j], temp);
+            perm(list, i+1, n);
+            SWAP(list[i], list[j], temp);
+        }
+    }
+
+}
+
+//순열을 생성하는 함수
+void perm_int(int *list, int i, int n) {
+    int j, temp;
+    if (i == n) {
+        for (j = 0; j <= n; j++) {
+            printf("%d", list[j]);
+        }
+        printf("   ");
+    }
+    else {
+        for(j=i; j<=n; j++) {
+            //j와 n의 값을 SWAP, 그리고 다시 SWAP으로 원상복구
+            SWAP(list[i], list[j], temp);
+            perm_int(list, i+1, n);
+            SWAP(list[i], list[j], temp);
+        }
+    }
+}
+
+int main(void)
+{
+    //2.1-------------------------------------------------------
+    /*
+    char A[4][10] = {"GO","BOY","GIRL","GIRLS"};
+    for(int i=0; i<4; i++) {
+        printf("string : %s\n",A[i]);
+        perm(A[i],0,strlen(A[i])-1);
+        printf("\n");
+    }
+    */
+    //2.2-------------------------------------------------------
+    while(1) {
+        int n=0;
+        int arr[33]={0, };
+        double duration;
+        clock_t start;
+
+        printf("1~n permutation (1 ~ 32) : ");
+        scanf("%d",&n);
+
+        if(n==-1) break;
+        if(n>32 || n<1) {
+            printf("Error\n");
+            continue;
+        }
+
+        for(int i=0; i<n; i++) {
+            arr[i]=i+1;
+        }
+        start = clock();
+        perm_int(arr,0,n-1);
+        printf("\n");
+
+        duration = ((double)(clock() - start)) / CLOCKS_PER_SEC;
+        printf("time : %f\n",duration);
+    }
+
+    //2.3-------------------------------------------------------
+
+    int cycle=0;
+    int time[]={0,};
+    printf("element_num    time\n");
+
+    while(cycle<=10) {
+        int arr[33]={0, };
+        double duration;
+        clock_t start;
+
+        for(int i=0; i<cycle; i++) {
+            arr[i]=i+1;
+        }
+        start = clock();
+        perm_int(arr,0,cycle-1);
+        printf("\n");
+
+        duration = ((double)(clock() - start)) / CLOCKS_PER_SEC;
+        printf("time : %f\n",duration);
+
+        time[cycle] = duration;
+        cycle++;
+
+    }
+    for(int i=1; i<=cycle; i++) {
+        printf("%d ",time[i]);
+    }
+
+
+}
